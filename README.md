d5efe5c53f4300a0c088b9a44e22001d2a3c523a
Gabriel Hayes <gabriel.speed.bullock@gmail.com>
2018-04-22 20:58:00 -0500

    Initial commit.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Web Packed Vue App
+
+> A Vue.js project using Jade and SCSS bundled by Webpack.
---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<!-- Latest compiled and minified CSS -->
+<template lang="jade">
+#container(style="text-align: center")
+  h1(class="#jumbotron") Vue App Web Packed!
+  p {{ testModel.name }} World!
+</template>
+<script>
+export default {
+  data() {
+
+    return {
+      greeting: "Hello",
+    testModel: {
+      name: "Bob",
+      occupation: "Worker"
+    }
+  }
+  },
+  created: function(){
+    this.testModel.name = "Rick";
+  }
+
+}
+
+</script>
+
+<style lang = "scss">
+@import url("https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css");
+</style>
